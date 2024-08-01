5cb5279a5cf4e025a4897911b07229fa350472cd
ravinsan <39220869+ravinsan@users.noreply.github.com>
2024-08-01 23:32:07 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,10 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <Provider store={store}>
     <ToastContainer/>
     <App />
     </Provider>
-  </React.StrictMode>,
+  //</React.StrictMode>,
 )
